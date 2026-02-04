7a4fb64978c4c4312bec323191ecc8b6d0d26bc5
James Stout <james.stout@gmail.com>
2026-02-04 13:26:11 -0500

    added app id

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'b03ktw7va8fx2pngboqirq3y',
   }
 })
